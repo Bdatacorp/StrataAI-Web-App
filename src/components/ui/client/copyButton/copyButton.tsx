67be1d81187d6b58720dc8116326fe48ae839053
I S Weerasingha <124188980+isweerasingha@users.noreply.github.com>
2024-08-10 01:06:08 +0530

    user chat ui changes

---
+++ src/components/ui/client/copyButton/copyButton.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { ActionIcon, CopyButton, rem, Tooltip } from "@mantine/core";
+import { FaCheck } from "react-icons/fa6";
+import { IoCopyOutline } from "react-icons/io5";
+
+export default function CopyButtonElement({ value }: { value?: string }) {
+  return (
+    <CopyButton value={value || ""} timeout={2000}>
+      {({ copied, copy }) => (
+        <Tooltip label={copied ? "Copied" : "Copy"} withArrow position="right">
+          <ActionIcon
+            color={copied ? "teal" : "gray"}
+            variant="subtle"
+            onClick={copy}
+          >
+            {copied ? (
+              <FaCheck style={{ width: rem(16) }} />
+            ) : (
+              <IoCopyOutline style={{ width: rem(16) }} />
+            )}
+          </ActionIcon>
+        </Tooltip>
+      )}
+    </CopyButton>
+  );
+}
--- src/components/ui/client/message/message.tsx
+++ src/components/ui/client/message/message.tsx
@@ -4,16 +4,46 @@ import {
   ClientMessage,
   MessageRoles,
 } from "@/components/modules/user/chat/types";
+import { ActionIcon, Code, CopyButton, Text, Tooltip } from "@mantine/core";
 import Markdown from "react-markdown";
+import CopyButtonElement from "../copyButton/copyButton";
+import { MdEmail } from "react-icons/md";
+import Image from "next/image";
 
 export default function ChatMessage(message: ClientMessage) {
   return (
     <div className="w-full">
       {message.role === MessageRoles.Assistant ? (
-        <div className="bg-white p-5 rounded">
-          <article className="prose ">
-            <Markdown>{message.text}</Markdown>
-          </article>
+        <div className="flex flex-col gap-2">
+          <div>
+            <Image
+              src="/img/strata-ai.png"
+              alt="Strata AI Assistant"
+              width={70}
+              height={40}
+            />
+          </div>
+
+          <div className="bg-white p-5 rounded flex flex-col gap-3">
+            <article className="prose ">
+              <Markdown>{message.text}</Markdown>
+            </article>
+            <div className="w-full flex justify-between items-center">
+              <div className=" flex gap-2">
+                <CopyButtonElement value={message.text} />
+
+                <Tooltip label="Contact Manager">
+                  <ActionIcon
+                    variant="transparent"
+                    color="gray"
+                    className="hover:bg-gray-100"
+                  >
+                    <MdEmail />
+                  </ActionIcon>
+                </Tooltip>
+              </div>
+            </div>
+          </div>
         </div>
       ) : (
         <div className="float-right bg-primary p-5 text-white rounded-3xl w-3/4 lg:w-7/12">
